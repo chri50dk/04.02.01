3f2f2b1c8c577ebcf9b4618aedfea8ce4da36c2e
chri50dk <chri50dk@stud.kea.dk>
2019-05-27 12:34:21 +0200

    SKEMA PÅBEGYNDT

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 window.addEventListener("load", sidenVises);
 
 function sidenVises() {
@@ -20,4 +21,15 @@ function toggleMenu() {
     } else {
         document.querySelector("#menuknap").textContent = "X";
     }
+}
+*/
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
 }
